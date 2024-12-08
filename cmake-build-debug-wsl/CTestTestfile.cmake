ad70aeb4e588a2f9967b9564bee6353ad43b7ca1
IvanTarasov <iwan.tarasow2013@gmail.com>
2024-12-08 18:07:12 +0300

    first

--- cmake-build-debug-wsl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ivan_kanga/univer/os/lab2
-# Build directory: /home/ivan_kanga/univer/os/lab2/cmake-build-debug-wsl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- test/test.cpp
+++ test/test.cpp
@@ -33,41 +33,7 @@ int main(int argc, char** args) {
 //    chdir(args[1]);
 //    InitGoogleTest(&argc, args);
 //    return RUN_ALL_TESTS();
-    const char *filename = "../example.txt";  // Имя файла
-    int fd = open(filename, O_RDWR);        // Открытие файла для записи и чтения
-    if (fd == -1) {
-        perror("open");
-        return 1;
-    }
 
-    // Получаем размер файла
-    off_t filesize = lseek(fd, 0, SEEK_END);
-
-    // Отображаем файл в память
-    void *mapped_data = mmap(NULL, 60, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (mapped_data == MAP_FAILED) {
-        perror("mmap");
-        close(fd);
-        return 1;
-    }
-
-    // Теперь данные файла отображены в память, и мы можем их изменять
-    printf("Original content: %s\n", (char *)mapped_data);
-    
-    // Изменяем данные в памяти
-    ftruncate(fd, strlen("Hello, mmap!") + 1);
-    strcpy((char *)mapped_data, "Hello, mmap!");
-
-    // Данные сразу записываются обратно в файл, так как используется MAP_SHARED
-    printf("Modified content: %s\n", (char *)mapped_data);
-
-    // Освобождаем память, когда больше не нужно
-    if (munmap(mapped_data, filesize) == -1) {
-        perror("munmap");
-    }
-
-    // Закрываем файл
-    close(fd);
 
     return 0;
 }
